fc1c01f88e84e5fca55edd2e51af741be8169405
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-05-21 22:08:09 +0300

    init

--- CircularProgressIndicator/ContentView.swift
+++ CircularProgressIndicator/ContentView.swift
@@ -9,8 +9,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var progress: Double = 0.0
+    
     var body: some View {
-        Text("Hello, World!")
+        ZStack {
+            Circle()
+                .stroke(lineWidth: 50)
+                .opacity(0.3)
+                .foregroundColor(Color.green)
+                .padding()
+            
+            Text(String(format: "%.0f%%", min(self.progress, 1) * 100))
+                .font(.system(size: 50))
+            
+            Circle()
+                .trim(from: 0.0, to: CGFloat(self.progress))
+                .stroke(style: StrokeStyle(lineWidth: 50, lineCap: .round, lineJoin: .round))
+                .foregroundColor(Color.green)
+                .animation(Animation.linear(duration: 2.0))
+                .padding()
+            
+            Button("Get Data") {
+                Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { timer in
+                    if self.progress >= 1 {
+                        self.progress = 0.0
+                    }
+                    self.progress += 0.1
+                    if self.progress >= 1 {
+                        timer.invalidate()
+                    }
+                }
+            }.offset(y: 300)
+            
+        }.padding()
     }
 }
 
